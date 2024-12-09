9e2859332c22dd62333efa1efe565bc09f0798d5
Pooky12332 <pooky12332@gmail.com>
2024-12-09 21:15:15 +0000

    Final Commit
    - Finished off the "TagReader" class (Question 3) and implemented a example of it working
    - Moved the menu function for the Airport class into itsself
    - Added a main method to the airport class and linked it to the menu
    - Deleted Main class from the project
    - Other misc changes.

--- src/Airport.java
+++ src/Airport.java
@@ -1,9 +1,36 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class Airport {
   static Queue<String> takeOffQueue = new LinkedList<>();
   static Queue<String> landQueue = new LinkedList<>();
+
+  // Main function that goes to menu
+  public static void main(String[] args) {
+    flightMenu();
+  }
+
+  // Function for menu
+  public static void flightMenu() {
+    Scanner kb = new Scanner(System.in);
+    Airport airport = new Airport();
+    System.out.print("> ");
+    String input = kb.nextLine();;
+
+    if (input.contains("takeoff")) {
+      takeoff(input.substring(8));
+    } else if (input.contains("land")) {
+      land(input.substring(5));
+    } else if (input.equals("next")) {
+      next();
+    } else if (input.equals("quit")) {
+      System.exit(0);
+    }
+
+    System.out.print("\n");
+    flightMenu();
+  }
 
   // Function to add to the takeoff queue
   public static void takeoff(String flightCode) {
--- src/TagReader.java
+++ src/TagReader.java
@@ -1,31 +1,70 @@
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
+import java.util.Stack;
 
 public class TagReader {
+  public static void main(String[] args) {
+    checkTags("index.html");
+  }
+
   public static void checkTags(String filename) {
+    boolean filePassed = true;
+
     try {
       File htmlFile = new File(filename);
       Scanner htmlScanner = new Scanner(htmlFile);
       while (htmlScanner.hasNextLine()) {
         String line = htmlScanner.nextLine();
-        indexLine(line);
+        filePassed = indexLine(line);
       }
     } catch (FileNotFoundException e) {
-      e.printStackTrace();
+      System.out.println("No file called " + filename + " found.");
+      System.exit(0);
     }
+
+    // Basic output depending on linePassed
+    if (filePassed) {
+      System.out.println("All tags in the given file are closed correctly.");
+    } else {
+      System.out.println("A tag in your file doesnt close. Double check your tags");
+    }
+    System.exit(0);
   }
 
-  public static void indexLine(String line) {
-    // Divide the line using the ">" char
-    
+  public static boolean indexLine(String line) {
+    Stack<String> tagStack = new Stack<String>();
+    // Splitting the line via the given space
+    String[] tags = line.split(" ");
 
-    // Place tags into an array
+    for (String tag : tags) {
+      // Checking to make sure the element is an actual tag
+      if ((tag.startsWith("<")) && (tag.endsWith(">"))) {
+        // Opening tag and closing tag logic
+        if (!tag.startsWith("</")) { // If opening tag:
+          if (!tag.equals("<br>")) {
+            tagStack.push(tag);
+          }
+        } else { // If closing tag:
+          // Check to see if there is a tag in the stack
+          String closingTag = tag.substring(2, tag.length() - 1);
 
+          if (tagStack.isEmpty()) {
+            return false;
+          }
 
-    // Check the tags array
-      // Index the open tags
-      // Index if we closed any open tags
+          // Since there are tags in the stack, we take the first one and check to see that they match
+          String openingTag = tagStack.pop();
+          String expectedTag = openingTag.substring(1, openingTag.length() - 1);
 
+          if (!expectedTag.equals(closingTag)) {
+            return false;
+          }
+        }
+      }
+    }
+
+    return tagStack.isEmpty();
   }
 }
